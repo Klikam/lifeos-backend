071965a92985d58fbe6ee3a3cdccf82ac54c8b96
Klikam <klikam.dev@gmail.com>
2026-06-08 14:59:47 +0200

    Add CORS for frontend calls

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
-import express, {type Express} from "express";
-import habitsRoutes from "./routes/habitsRoutes";
-import usersRouter from "./routes/usersRoutes"
-import {errorHandler} from "./middlewares/errorHandler.js";
-import {requestLogger} from "./middlewares/reqResLogger.js";
-import {toNodeHandler} from "better-auth/node";
-import {auth} from "./lib/auth";
+import express, { type Express } from 'express';
+import habitsRoutes from './routes/habitsRoutes';
+import usersRouter from './routes/usersRoutes';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { requestLogger } from './middlewares/reqResLogger.js';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
+import frontendCors from './middlewares/cors';
 
 const app: Express = express();
+
+// Configure CORS middleware
+app.use(frontendCors);
 
 // Auth -> must be before express.json()
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
-app.use(express.json())
+app.use(express.json());
 
 // Logger -> must be before routes
 app.use(requestLogger);
 
 // Routes
-app.use("/api/habits", habitsRoutes)
-app.use("/api/users", usersRouter)
+app.use('/api/habits', habitsRoutes);
+app.use('/api/users', usersRouter);
 
 // Global error handling
-app.use(errorHandler)
+app.use(errorHandler);
 
-export default app
+export default app;
